1d3b1d60708159ff2717f9e37d2963c31b8edc3a
Viral Joshi <viral@linxsystems.com>
2017-08-07 17:09:22 -0500

    updated

--- js/app.js
+++ js/app.js
@@ -1,227 +1,132 @@
-// 'use strict';
+'use strict';
 
-/*jshint strict:false */
 /*global google */
 /*global Google */
 /*global alert */
 
-// Initial locations with latitude and longitude
-var initialLocations = [
-    {
-        name: 'Quad (Illinois State University)',
-        lat: 40.5086536,
-        long: -88.9912084
-    },
-    {
-        name: 'The Rock Restaurant',
-        lat: 40.5092357,
-        long: -88.9860323
-    },
-    {
-        name: 'D P Dough',
-        lat: 40.5095144,
-        long: -88.9831754
-    },
-    {
-        name: 'Hancock Stadium',
-        lat: 40.5124877,
-        long: -88.9966437
-    },
-    {
-        name: 'Illinois State University Athletics',
-        lat: 40.5106554,
-        long: -88.9988456
-    },
-    {
-        name: 'Bone Student Center',
-        lat: 40.5112643,
-        long: -88.9924279
-    },
-    {
-        name: 'ISU Bowling and Billards Center',
-        lat: 40.512066,
-        long: -88.9909766
-    },
-    {
-        name: 'The Alamo II',
-        lat: 40.5091633,
-        long: -88.9890882
-    },
-    {
-        name: 'Maggie Miley\'s',
-        lat: 40.5098793,
-        long: -88.9833566
-    },
-    {
-        name: 'Insomnia Cookies',
-        lat: 40.5095127,
-        long: -88.9832346
-    },
-    {
-        name: 'Firehouse Pizza & Pub',
-        lat: 40.5093929,
-        long: -88.9834364
-    },
-    {
-        name: 'Emack & Bolio\'s Ice Cream',
-        lat: 40.5093929,
-        long: -88.9834364
-    },
 
-
-];
-
-// Declaring global variables now to satisfy strict mode
-var map;
-var clientID;
-var clientSecret;
-
-
-function formatPhone(phonenum) {
-    var regexObj = /^(?:\+?1[-. ]?)?(?:\(?([0-9]{3})\)?[-. ]?)?([0-9]{3})[-. ]?([0-9]{4})$/;
-    if (regexObj.test(phonenum)) {
-        var parts = phonenum.match(regexObj);
-        var phone = "";
-        if (parts[1]) {
-            phone += "+1 (" + parts[1] + ") ";
-        }
-        phone += parts[2] + "-" + parts[3];
-        return phone;
+//To get formatted phone number
+function fp(t) {
+    var e = /^(?:\+?1[-. ]?)?(?:\(?([0-9]{3})\)?[-. ]?)?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if (e.test(t)) {
+        var n = t.match(e),
+            o = "";
+        return n[1] && (o += "+1 (" + n[1] + ") "), o += n[2] + "-" + n[3]
     }
-    else {
-        //invalid phone number
-        return phonenum;
-    }
+    return t
 }
 
-var Location = function (data) {
-    var self = this;
-    this.name = data.name;
-    this.lat = data.lat;
-    this.long = data.long;
-    this.URL = "";
-    this.street = "";
-    this.city = "";
-    this.phone = "";
-
-    this.visible = ko.observable(true);
-
-    var foursquareURL = 'https://api.foursquare.com/v2/venues/search?ll=' + this.lat + ',' + this.long + '&client_id=' + clientID + '&client_secret=' + clientSecret + '&v=20160118' + '&query=' + this.name;
-
-    $.getJSON(foursquareURL).done(function (data) {
-        var results = data.response.venues[0];
-
-        if (typeof results === 'undefined') {
-
-        } else {
-            self.URL = results.url;
-            if (typeof self.URL === 'undefined') {
-                self.URL = "";
-            }
-            self.street = results.location.formattedAddress[0];
-            self.city = results.location.formattedAddress[1];
-            self.phone = results.contact.phone;
-            if (typeof self.phone === 'undefined') {
-                self.phone = "";
-            } else {
-                self.phone = formatPhone(self.phone);
-            }
+//View Model for the app to initialize clientID, clientSecret and load google map in the screen
+function vmfa() {
+    var t = this;
+    this.searchTerm = ko.observable(""), this.locList = ko.observableArray([]), map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 13,
+        center: {
+            lat: 40.5074476,
+            lng: -89.0000705
         }
+    }), cID = "JW3WFHDY3JDN40HQMJQBBTY0T53CF0PZVFXE5TJLXGEU2PES", cSec = "X5HA15N4QZVSUE1HBZPIMAF2RRQXBZEODIHRMR4WXAKVF22W", iloc.forEach(function (e) {
+        t.locList.push(new Location(e))
+    }), this.filteredList = ko.computed(function () {
+        var e = t.searchTerm().toLowerCase();
+        return e ? ko.utils.arrayFilter(t.locList(), function (t) {
+                var n = t.name.toLowerCase(),
+                    o = n.search(e) >= 0;
+                return t.visible(o), o
+            }) : (t.locList().forEach(function (t) {
+                t.visible(!0)
+            }), t.locList())
+    }, t), this.mapElem = document.getElementById("map"), this.mapElem.style.height = window.innerHeight - 50
+}
 
-    }).fail(function () {
-        alert("There was an error with the Foursquare API call. Please refresh the page and try again to load Foursquare data.");
-    });
+//`run` function to start from Google Map API and run `vmfa` function
+function run() {
+    ko.applyBindings(new vmfa)
+}
 
-    this.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
-        '<div class="content"><a href="' + self.URL + '">' + self.URL + "</a></div>" +
-        '<div class="content">' + self.street + "</div>" +
-        '<div class="content">' + self.city + "</div>" +
-        '<div class="content">' + self.phone + "</div></div>";
 
-    this.infoWindow = new google.maps.InfoWindow({content: self.contentString});
+//String formatter : To format string with replacing characters/words
+String.prototype.format || (String.prototype.format = function () {
+    var t = arguments;
+    return this.replace(/{(\d+)}/g, function (e, n) {
+        return void 0 !== t[n] ? t[n] : e
+    })
+});
 
-    this.marker = new google.maps.Marker({
-        position: new google.maps.LatLng(data.lat, data.long),
-        map: map,
-        title: data.name
-    });
+//initial location information
+var iloc = [{
+        "name": "Quad (Illinois State University)",
+        "lat": 40.5086536,
+        "long": -88.9912084
+    }, {
+        "name": "The Rock Restaurant",
+        "lat": 40.5092357,
+        "long": -88.9860323
+    }, {
+        "name": "D P Dough",
+        "lat": 40.5095144,
+        "long": -88.9831754
+    }, {
+        "name": "Hancock Stadium",
+        "lat": 40.5124877,
+        "long": -88.9966437
+    }, {
+        "name": "Illinois State University Athletics",
+        "lat": 40.5106554,
+        "long": -88.9988456
+    }, {
+        "name": "Bone Student Center",
+        "lat": 40.5112643,
+        "long": -88.9924279
+    }, {
+        "name": "ISU Bowling and Billards Center",
+        "lat": 40.512066,
+        "long": -88.9909766
+    }, {
+        "name": "The Alamo II",
+        "lat": 40.5091633,
+        "long": -88.9890882
+    }, {
+        "name": "Maggie Miley's",
+        "lat": 40.5098793,
+        "long": -88.9833566
+    }, {
+        "name": "Insomnia Cookies",
+        "lat": 40.5095127,
+        "long": -88.9832346
+    }, {
+        "name": "Firehouse Pizza & Pub",
+        "lat": 40.5093929,
+        "long": -88.9834364
+    }, {
+        "name": "Emack & Bolio's Ice Cream",
+        "lat": 40.5093929,
+        "long": -88.9834364
+    }],
 
-    this.showMarker = ko.computed(function () {
-        if (this.visible() === true) {
-            this.marker.setMap(map);
-        } else {
-            this.marker.setMap(null);
+    //Global vars and Location function to display information from Foursquare about location when location is selected 
+    map, cID, cSec, Location = function (t) {
+        var e = this;
+        this.name = t.name, this.lat = t.lat, this.long = t.long, this.URL = "", this.street = "", this.city = "", this.phone = "", this.visible = ko.observable(!0);
+        var n = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v=20160118&query={4}".format(this.lat, this.long, cID, cSec, this.name);
+        $.getJSON(n).done(function (t) {
+            var n = t.response.venues[0];
+            void 0 === n || (e.URL = n.url, void 0 === e.URL && (e.URL = ""), e.street = n.location.formattedAddress[0], e.city = n.location.formattedAddress[1], e.phone = n.contact.phone, e.phone = void 0 === e.phone ? "" : fp(e.phone))
+        }).fail(function () {
+            alert("Foursquare API call ran into error. To load Foursquare data, please refresh page.")
+        }), this.contentString = '<div class="info-window-content"><div class="title"><b>' + t.name + '</b></div><div class="content"><a href="' + e.URL + '">' + e.URL + '</a></div><div class="content">' + e.street + '</div><div class="content">' + e.city + '</div><div class="content">' + e.phone + "</div></div>", this.infoWindow = new google.maps.InfoWindow({
+            content: e.contentString
+        }), this.marker = new google.maps.Marker({
+            position: new google.maps.LatLng(t.lat, t.long),
+            map: map,
+            title: t.name
+        }), this.showMarker = ko.computed(function () {
+            return this.marker.setMap(this.visible() === !0 ? map : null), !0
+        }, this), this.marker.addListener("click", function () {
+            e.contentString = '<div class="info-window-content"><div class="title"><b>' + t.name + '</b></div><div class="content"><a href="' + e.URL + '">' + e.URL + '</a></div><div class="content">' + e.street + '</div><div class="content">' + e.city + '</div><div class="content"><a href="tel:' + e.phone + '">' + e.phone + "</a></div></div>", e.infoWindow.setContent(e.contentString), e.infoWindow.open(map, this), e.marker.setAnimation(google.maps.Animation.BOUNCE), setTimeout(function () {
+                e.marker.setAnimation(null)
+            }, 2100)
+        }), this.bounce = function () {
+            google.maps.event.trigger(e.marker, "click")
         }
-        return true;
-    }, this);
-
-    this.marker.addListener('click', function () {
-        self.contentString = '<div class="info-window-content"><div class="title"><b>' + data.name + "</b></div>" +
-            '<div class="content"><a href="' + self.URL + '">' + self.URL + "</a></div>" +
-            '<div class="content">' + self.street + "</div>" +
-            '<div class="content">' + self.city + "</div>" +
-            '<div class="content"><a href="tel:' + self.phone + '">' + self.phone + "</a></div></div>";
-
-        self.infoWindow.setContent(self.contentString);
-
-        self.infoWindow.open(map, this);
-
-        self.marker.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function () {
-            self.marker.setAnimation(null);
-        }, 2100);
-    });
-
-    this.bounce = function (place) {
-        google.maps.event.trigger(self.marker, 'click');
     };
-};
-
-function AppViewModel() {
-    var self = this;
-
-    this.searchTerm = ko.observable("");
-
-    this.locationList = ko.observableArray([]);
-
-    map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 15,
-        center: {lat: 40.5082864, lng: -88.990047}
-    });
-
-    // Foursquare API settings
-    clientID = "JW3WFHDY3JDN40HQMJQBBTY0T53CF0PZVFXE5TJLXGEU2PES";
-    clientSecret = "X5HA15N4QZVSUE1HBZPIMAF2RRQXBZEODIHRMR4WXAKVF22W";
-
-    initialLocations.forEach(function (locationItem) {
-        self.locationList.push(new Location(locationItem));
-    });
-
-    this.filteredList = ko.computed(function () {
-        var filter = self.searchTerm().toLowerCase();
-        if (!filter) {
-            self.locationList().forEach(function (locationItem) {
-                locationItem.visible(true);
-            });
-            return self.locationList();
-        } else {
-            return ko.utils.arrayFilter(self.locationList(), function (locationItem) {
-                var string = locationItem.name.toLowerCase();
-                var result = (string.search(filter) >= 0);
-                locationItem.visible(result);
-                return result;
-            });
-        }
-    }, self);
-
-    this.mapElem = document.getElementById('map');
-    this.mapElem.style.height = window.innerHeight - 50;
-}
-
-function startApp() {
-    ko.applyBindings(new AppViewModel());
-}
-
-function errorHandling() {
-    alert("Google Maps has failed to load. Please check your internet connection and try again.");
-}
